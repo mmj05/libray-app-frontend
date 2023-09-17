e6196613c32bf8eb99c64b617d6b1409399ce0b1
Muhaimin Jobayer <mmj0993@gmail.com>
2023-09-17 01:58:04 -0500

    complete homepage components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
-import { Carousel } from './layouts/Carousel/Carousel';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { HomePage } from './layouts/HomePage/HomePage';
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <ExploreTopBooks />
-      <Carousel />
-    </>
-
-  );
+export const App = () => {
+    return (
+        <div>
+            <Navbar />
+            <HomePage />
+            <Footer />
+        </div>
+    );
 }
 
-export default App;
