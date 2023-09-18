3b656016c310d01caddf63442c82b23ab0d33e9c
Muhaimin Jobayer <mmj0993@gmail.com>
2023-09-18 15:45:55 -0500

    Leverage SpinneLoading component

--- src/layouts/HomePage/components/Carousel.tsx
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -1,6 +1,7 @@
 import { ReturnBook } from './ReturnBook';
 import { useState, useEffect } from 'react';
 import BookModel from '../../../models/BookModel';
+import { SpinnerLoading } from '../../Utils/SpinnerLoading';
 
 export const Carousel = () => {
 
@@ -47,9 +48,7 @@ export const Carousel = () => {
 
     if (isLoading) {
         return (
-            <div className='container m-5'>
-                <p>Loading...</p>
-            </div>
+            <SpinnerLoading />
         );
     }
 
