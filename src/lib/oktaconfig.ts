1b905bbc0baa90edace740e39cd866db06f50603
Muhaimin Jobayer <mmj0993@gmail.com>
2023-09-21 13:26:09 -0500

    Fix spelling error

--- src/lib/oktaconfig.ts
+++ src/lib/oktaconfig.ts
@@ -1,4 +1,4 @@
-export const oktaconfig = {
+export const oktaConfig = {
     clientId: '0oabg52vxmltivr2c5d7',
     issuer: 'https://dev-14309690.okta.com/oauth2/default',
     redirectUri: 'http://localhost:3000/login/callback',
