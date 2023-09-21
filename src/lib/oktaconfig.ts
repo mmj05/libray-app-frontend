c6235775cd5371f4d2262bd827f6b8dcfaaec818
Muhaimin Jobayer <mmj0993@gmail.com>
2023-09-21 11:28:58 -0500

    Set up okta configuration

---
+++ src/lib/oktaconfig.ts
@@ -0,0 +1,8 @@
+export const oktaconfig = {
+    clientId: '0oabg52vxmltivr2c5d7',
+    issuer: 'https://dev-14309690.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true
+}
