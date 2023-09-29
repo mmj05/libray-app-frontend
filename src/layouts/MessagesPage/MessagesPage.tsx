19119d5f24bbb30fee0eb466294830f2096c3d55
Muhaimin Jobayer <mmj0993@gmail.com>
2023-09-28 22:54:36 -0500

    Render PostNewMessage component

--- src/layouts/MessagesPage/MessagesPage.tsx
+++ src/layouts/MessagesPage/MessagesPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { PostNewMessage } from './components/PostNewMessage';
 
 export const MessagesPage = () => {
     const [messagesClick, setMessagesClick] = useState(false);
@@ -43,7 +44,7 @@ export const MessagesPage = () => {
                         role='tabpanel'
                         aria-labelledby='nav-send-message-tab'
                     >
-                        <p>Post new message</p>
+                       <PostNewMessage />
                     </div>
                     <div
                         className='tab-pane fade'
