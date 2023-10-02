8bc454b5df1160c62ff9b40bb7df6f4844f4715e
Muhaimin Jobayer <mmj0993@gmail.com>
2023-10-02 00:23:00 -0500

    Configure for HTTPS

--- src/lib/oktaconfig.ts
+++ src/lib/oktaconfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oabg52vxmltivr2c5d7',
     issuer: 'https://dev-14309690.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: 'https://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true
