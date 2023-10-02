b85362d4a5fef0b4f3e1c2b9427de3e6d521ab92
Muhaimin Jobayer <mmj0993@gmail.com>
2023-10-02 01:49:39 -0500

    Repleace hardcoded base URLs with .env's REACT_APP_API variable

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -37,7 +37,7 @@ export const BookCheckoutPage = () => {
 
     useEffect(() => {
         const fetchBooks = async () => {
-            const baseUrl: string = `http://localhost:8080/api/books/${bookId}`;
+            const baseUrl: string = `${process.env.REACT_APP_API}/books/${bookId}`;
 
             const response = await fetch(baseUrl);
 
@@ -70,7 +70,7 @@ export const BookCheckoutPage = () => {
 
     useEffect(() => {
         const fetchBookReviews = async () => {
-            const reviewUrl: string = `http://localhost:8080/api/reviews/search/findByBookId?bookId=${bookId}`;
+            const reviewUrl: string = `${process.env.REACT_APP_API}/reviews/search/findByBookId?bookId=${bookId}`;
             const responseReviews = await fetch(reviewUrl);
 
             if (!responseReviews.ok) {
@@ -120,7 +120,7 @@ export const BookCheckoutPage = () => {
     useEffect(() => {
         const fetchUserReviewBook = async () => {
             if (authState && authState.isAuthenticated) {
-                const url = `http://localhost:8080/api/reviews/secure/user/book/?bookId=${bookId}`;
+                const url = `${process.env.REACT_APP_API}/reviews/secure/user/book/?bookId=${bookId}`;
                 const requestOptions = {
                     method: 'GET',
                     headers: {
@@ -147,7 +147,7 @@ export const BookCheckoutPage = () => {
     useEffect(() => {
         const fetchUserCurrentLoansCount = async () => {
             if (authState && authState.isAuthenticated) {
-                const url = `http://localhost:8080/api/books/secure/currentloans/count`;
+                const url = `${process.env.REACT_APP_API}/books/secure/currentloans/count`;
                 const requestOptions = {
                     method: 'GET',
                     headers: {
@@ -178,7 +178,7 @@ export const BookCheckoutPage = () => {
     useEffect(() => {
         const fetchUserCheckedOutBook = async () => {
             if (authState && authState.isAuthenticated) {
-                const url = `http://localhost:8080/api/books/secure/ischeckedout/byuser/?bookId=${bookId}`;
+                const url = `${process.env.REACT_APP_API}/books/secure/ischeckedout/byuser/?bookId=${bookId}`;
                 const requestOptions = {
                     method: 'GET',
                     headers: {
@@ -222,7 +222,7 @@ export const BookCheckoutPage = () => {
     }
 
     async function checkoutBook() {
-        const url = `http://localhost:8080/api/books/secure/checkout/?bookId=${bookId}`;
+        const url = `${process.env.REACT_APP_API}/books/secure/checkout/?bookId=${bookId}`;
         const requestOptions = {
             method: 'PUT',
             headers: {
@@ -248,7 +248,7 @@ export const BookCheckoutPage = () => {
             bookId,
             reviewDescription
         );
-        const url = `http://localhost:8080/api/reviews/secure`;
+        const url = `${process.env.REACT_APP_API}/reviews/secure`;
         const requestOptions = {
             method: 'POST',
             headers: {
--- src/layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
+++ src/layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
@@ -21,7 +21,7 @@ export const ChangeQuantityOfBook: React.FC<{
     }, []);
 
     async function increaseQuantity() {
-        const url = `http://localhost:8080/api/admin/secure/increase/book/quantity/?bookId=${props.book?.id}`;
+        const url = `${process.env.REACT_APP_API}/admin/secure/increase/book/quantity/?bookId=${props.book?.id}`;
         const requestOptions = {
             method: 'PUT',
             headers: {
@@ -40,7 +40,7 @@ export const ChangeQuantityOfBook: React.FC<{
     }
 
     async function decreaseQuantity() {
-        const url = `http://localhost:8080/api/admin/secure/decrease/book/quantity/?bookId=${props.book?.id}`;
+        const url = `${process.env.REACT_APP_API}/admin/secure/decrease/book/quantity/?bookId=${props.book?.id}`;
         const requestOptions = {
             method: 'PUT',
             headers: {
@@ -59,7 +59,7 @@ export const ChangeQuantityOfBook: React.FC<{
     }
 
     async function deleteBook() {
-        const url = `http://localhost:8080/api/admin/secure/delete/book/?bookId=${props.book?.id}`;
+        const url = `${process.env.REACT_APP_API}/admin/secure/delete/book/?bookId=${props.book?.id}`;
         const requestOptions = {
             method: 'DELETE',
             headers: {
