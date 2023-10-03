7e5442196a1b0864ff9ec5d3721681088d08953d
Muhaimin Jobayer <mmj0993@gmail.com>
2023-10-03 15:36:20 -0500

    Wrap <App> component with <Elements> from stripe

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe(
+    'pk_test_51NwtSSA6N2XryKBX3Br6bsSlD7do9s6YMgb00IVAWBVQOiSmBbGA9OmibYkzMHafelcrCXMNNiZCjAA1zLVtX1Nz00w5AInhvu'
+);
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <BrowserRouter>
-        <App />
+        <Elements stripe={stripePromise}>
+            <App />
+        </Elements>
     </BrowserRouter>
 );
