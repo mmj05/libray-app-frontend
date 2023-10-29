5c34a826ac8c1227becdec5ac2b2c8a5d6a14a7d
muhaiminjobayer <mmj0993@gmail.com>
2023-10-29 14:18:50 -0500

    Utilized vercel env variable

--- src/lib/oktaconfig.ts
+++ src/lib/oktaconfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oabg52vxmltivr2c5d7',
     issuer: 'https://dev-14309690.okta.com/oauth2/default',
-    redirectUri: 'https://library-app-pearl.vercel.app/login/callback',
+    redirectUri: `https://${process.env.VERCEL_URL}/login/callback`,
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true
