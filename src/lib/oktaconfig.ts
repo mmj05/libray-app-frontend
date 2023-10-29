e7f5cd8160252a142f29c07103d8e824631d4bd4
muhaiminjobayer <mmj0993@gmail.com>
2023-10-29 04:09:21 -0500

    Minor change

--- src/lib/oktaconfig.ts
+++ src/lib/oktaconfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oabg52vxmltivr2c5d7',
     issuer: 'https://dev-14309690.okta.com/oauth2/default',
-    redirectUri: 'https://library-abli7uh9q-mmj05.vercel.app/login/callback',
+    redirectUri: 'https://library-app-pearl.vercel.app/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true
