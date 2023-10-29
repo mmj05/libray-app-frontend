08394fdc6fbc81d60a8baa78b92fc616433dca90
muhaiminjobayer <mmj0993@gmail.com>
2023-10-29 04:06:33 -0500

    Oktaconfig change

--- src/lib/oktaconfig.ts
+++ src/lib/oktaconfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oabg52vxmltivr2c5d7',
     issuer: 'https://dev-14309690.okta.com/oauth2/default',
-    redirectUri: 'https://localhost:3000/login/callback',
+    redirectUri: 'https://library-abli7uh9q-mmj05.vercel.app/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true
