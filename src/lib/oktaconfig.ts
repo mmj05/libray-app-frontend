511206bfe5fe9523a0e7d600bc9cf70096dc8d99
muhaiminjobayer <mmj0993@gmail.com>
2023-11-05 12:19:56 -0600

    Changed import for oktaConfig

--- src/lib/oktaconfig.ts
+++ src/lib/oktaconfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oabg52vxmltivr2c5d7',
     issuer: 'https://dev-14309690.okta.com/oauth2/default',
-    redirectUri: `https://${process.env.VERCEL_URL}/login/callback`,
+    redirectUri: `https://love-to-read.onrender.com/login/callback`,
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true
