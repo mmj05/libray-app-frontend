0ebb79592b6d48f1cb7c7b7efafe0431c8ad78ec
muhaiminjobayer <mmj0993@gmail.com>
2023-11-10 19:10:20 -0600

    Updated oktaConfig

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
     issuer: process.env.REACT_APP_OKTA_ISSUER,
-    redirectUri: 'https://love-to-read.onrender.com/login/callback',
+    redirectUri: 'https://library-app-pearl.vercel.app/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true
